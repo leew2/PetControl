5c1b14c0d98f4c64fde1396345927a39875c6a3d
Wang <127792775+leew2@users.noreply.github.com>
2023-11-05 01:58:33 -0600

    Should have everything required in 'release 2' plus some quality of life changes

--- src/main/java/framework/pet/Pet.java
+++ src/main/java/framework/pet/Pet.java
@@ -25,14 +25,10 @@ public class Pet {
                 break;
             }
             switch(user){
-                case 1:
-                    showPet();
-                    break;
-                case 2:
-                    addPet();
-                    break;
-                default:
-                    System.out.println("Not In Option");
+                case 1 -> showPet();
+                case 2 -> addPet();
+                case 5 -> searchPet();
+                default -> System.out.println("Not In Option");
             }
             
             
@@ -46,7 +42,7 @@ public class Pet {
         System.out.println("Enter 'Done' to exit");
         while(true){
             
-            System.out.print("(Name) (Age)");
+            System.out.print("(Name) (Age): ");
             String petName = input.nextLine();
             String[] nameAge = petName.split(" ");
             if("done".equals(nameAge[0])){
@@ -57,12 +53,29 @@ public class Pet {
         }
     }
     
+    public static void searchPet(){
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter A Name To Search: ");
+        String name = input.nextLine();
+        
+        showBreaker();
+        System.out.printf("%s %2s %s %-10s %s %3s %s", "|","ID","|","Name","|","Age","|");
+        showBreaker();
+        for(int i =0; i < collection.size(); i++){
+            if(collection.get(i).getName().equals(name)){
+                System.out.printf("%s %2s %s %-10s %s %3s %s%n", "|",i,"|",collection.get(i).getName(),"|",collection.get(i).getAge(),"|");
+            }
+        }
+        showBreaker();
+        
+    }
+    
     public static void showMenu(){
         System.out.println("1) View All Pets");
         System.out.println("2) Add More Pets");
+        System.out.println("5) Search For Pets");
         System.out.println("7) Exit");
-    }
-    
+    }    
     
     public static void showPet(){
         showBreaker();
@@ -71,7 +84,7 @@ public class Pet {
         for(int i =0; i < collection.size(); i++){
             System.out.printf("%s %2s %s %-10s %s %3s %s%n", "|",i,"|",collection.get(i).getName(),"|",collection.get(i).getAge(),"|");
         }
-        
+        showBreaker();
     }
     
     public static void showBreaker(){
