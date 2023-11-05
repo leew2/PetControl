bbf0806d4dc473743cf9679f05e3f48e70c2c560
Wang <127792775+leew2@users.noreply.github.com>
2023-11-05 01:43:42 -0600

    Should have everything required in 'release 1'

--- src/main/java/framework/pet/Pet.java
+++ src/main/java/framework/pet/Pet.java
@@ -5,13 +5,76 @@
 
 package framework.pet;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 /**
  *
  * @author WangL
  */
 public class Pet {
-
+    private static ArrayList<petObj> collection = new ArrayList(); 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner menuScan = new Scanner(System.in);
+        int user;
+        while(true){
+            showMenu();
+            user = menuScan.nextInt();
+            System.out.println("Your Choice: " + user);
+            if(user == 7){
+                break;
+            }
+            switch(user){
+                case 1:
+                    showPet();
+                    break;
+                case 2:
+                    addPet();
+                    break;
+                default:
+                    System.out.println("Not In Option");
+            }
+            
+            
+        }   //  End of (Main)While loop
+    }   //  End of Main class
+    
+    public static void addPet(){
+        Scanner input = new Scanner(System.in);
+        petObj pet;
+        
+        System.out.println("Enter 'Done' to exit");
+        while(true){
+            
+            System.out.print("(Name) (Age)");
+            String petName = input.nextLine();
+            String[] nameAge = petName.split(" ");
+            if("done".equals(nameAge[0])){
+                break;
+            }
+            pet = new petObj(nameAge[0], Integer.parseInt(nameAge[1]));
+            collection.add(pet);
+        }
+    }
+    
+    public static void showMenu(){
+        System.out.println("1) View All Pets");
+        System.out.println("2) Add More Pets");
+        System.out.println("7) Exit");
+    }
+    
+    
+    public static void showPet(){
+        showBreaker();
+        System.out.printf("%s %2s %s %-10s %s %3s %s", "|","ID","|","Name","|","Age","|");
+        showBreaker();
+        for(int i =0; i < collection.size(); i++){
+            System.out.printf("%s %2s %s %-10s %s %3s %s%n", "|",i,"|",collection.get(i).getName(),"|",collection.get(i).getAge(),"|");
+        }
+        
+    }
+    
+    public static void showBreaker(){
+        System.out.printf("%n%s%23s%s%n", "+","-----------------------","+");
     }
 }
---
+++ src/main/java/framework/pet/petObj.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package framework.pet;
+
+/**
+ *
+ * @author WangL
+ */
+public class petObj {
+    private String name;
+    private int age;
+    
+    public petObj(String name, int age){
+        this.name = name;
+        this.age = age;
+    }
+    
+    
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+    
+
+}
